f10d9934f2d183030311feb92c5655775dfff408
Olga Tsiouri <olgatsiouri@outlook.com>
2025-01-09 19:16:14 +0200

    .

--- src/Multi FASTA Splitter.py
+++ src/Multi FASTA Splitter.py
@@ -13,7 +13,7 @@ def run_pipeline(input_file, progress_bar):
     input_directory = os.path.dirname(input_file)
 
     # Select input file
-    infile = os.path.basename(input_file)
+    infile = str(os.path.basename(input_file)).replace(" ","\ ")
 
     # Output folder
     output_directory = f"{input_file}.split"
